37948adbc57dc6632d69a0eed56b56a23903fdd8
Ryutaro Mori <forest19951016@gmail.com>
2025-05-21 01:57:56 +0900

    add test_model

--- day5/演習2/black_check.py
+++ day5/演習2/black_check.py
@@ -1,19 +1,7 @@
 # black black_check.pyで整形してくれる
-def say_hello(name):
-    print("Hello," + name + "!")  # greet
-
-
-def say_hello(name):
-    print("Hello," + name + "!")  # greet
-
-
+def say_hello(name):print("Hello,"+name+"!")   # greet
+def say_hello(name):print("Hello,"     +    name   +"!")   # greet
+def add( a,b):return a+b
+def add( a ,     b    ):return a+b
 def add(a, b):
-    return a + b
-
-
-def add(a, b):
-    return a + b
-
-
-def add(a, b):
-    return a + b
+    return a+b
